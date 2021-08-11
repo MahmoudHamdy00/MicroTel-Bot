80c552d4d25818ce76713476dcdb5ec287fc2329
MahmoudHamdy00 <mahmoudh.morsy@gmail.com>
2021-08-11 16:24:39 +0200

    implement national id custom action
    - verify national id custom action
    - modify verifyNationaIid dialog

--- DatabaseCustomActions/VerifyNationalIDDialogBotComponent.cs
+++ DatabaseCustomActions/VerifyNationalIDDialogBotComponent.cs
@@ -3,12 +3,12 @@ using Microsoft.Bot.Builder.Dialogs.Declarative;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-public class AddUserDialogBotComponent : BotComponent
+public class VerifyNationalIDDialogBotComponent : BotComponent
 {
     public override void ConfigureServices(IServiceCollection services, IConfiguration configuration)
     {
         // Anything that could be done in Startup.ConfigureServices can be done here.
         // In this case, the MultiplyDialog needs to be added as a new DeclarativeType.
-        services.AddSingleton<DeclarativeType>(sp => new DeclarativeType<AddUser>(AddUser.Kind));
+        services.AddSingleton<DeclarativeType>(sp => new DeclarativeType<VerifyNationalID>(VerifyNationalID.Kind));
     }
 }

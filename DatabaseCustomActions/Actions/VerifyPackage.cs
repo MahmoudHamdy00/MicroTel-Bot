f631b61c9678574483b189f54153ba0b11e82327
ahmedmeshref <a.meshref@alustudent.com>
2021-09-05 14:16:20 +0200

    Refrence connection string from all custom actions

--- DatabaseCustomActions/Actions/VerifyPackage.cs
+++ DatabaseCustomActions/Actions/VerifyPackage.cs
@@ -8,7 +8,7 @@ using AdaptiveExpressions.Properties;
 using DatabaseCustomActions;
 using Microsoft.Bot.Builder.Dialogs;
 using Newtonsoft.Json;
-using static DatabaseCustomActions.helperFunctions;
+using static DatabaseCustomActions.HelperFunctions;
 
 public class VerifyPackage : Dialog
 {
@@ -46,7 +46,10 @@ public class VerifyPackage : Dialog
 
     public override Task<DialogTurnResult> BeginDialogAsync(DialogContext dc, object options = null, CancellationToken cancellationToken = default(CancellationToken))
     {
-        string connectionString = "Server=tcp:microtel.database.windows.net,1433;Initial Catalog=microtel-db;Persist Security Info=False;User ID=ahmed;Password=123456#Mahmoud;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        // Extract connection string from env variables 
+        EnvironmentVariables env = new EnvironmentVariables();
+        string connectionString = env.connectionString;
+
         var data = PackageName.GetValue(dc.State);
 
         SqlConnection conn = new SqlConnection(connectionString);
